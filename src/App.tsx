c57493c62a802120bc725a9d514c91dc07cc514a
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2024-03-13 22:59:30 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const binanceWebSocketUrl = "wss://stream.binance.com:9443/ws/ethusdt@depth";
+
+  const ws = new WebSocket(binanceWebSocketUrl);
+
+  ws.onopen = () => {
+    console.log("Connected to Binance WebSocket API");
+  };
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    const { b, a } = data;
+
+    const buyPrice = parseFloat(b[0][0]);
+    console.log(buyPrice);
+    const sellPrice = parseFloat(a[0][0]);
+    console.log(sellPrice);
+  };
+
+  ws.onclose = () => {
+    console.log("Disconnected from Binance WebSocket API");
+  };
+
+  ws.onerror = (error: Event) => {
+    console.error("WebSocket error:", error);
+  };
+
+  return <></>;
 }
 
 export default App;
